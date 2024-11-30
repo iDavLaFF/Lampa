ae6d7ef2809c698c2fb5f0c810987c0c8b3aefcb
iDavLaFF <idavlaff@ya.ru>
2024-12-01 01:43:18 +0300

    Update WEB.js

--- WEB.js
+++ WEB.js
@@ -15,7 +15,7 @@
         'https://nb557.github.io/plugins/rating.js',
 // @LAMPAC thanks. !NB Timecode work only in torrents
         'https://idavlaff.github.io/lampa/plugins/timecode.js',
-// @CUB.RED thanks. Combine 'interface' & 'cardify' plugins => 'beautify' !NB 'cardify' without trailers, only cards beauty
+// @CUB thanks, !'but pay for a big background, seriously?' Combine 'interface' & 'cardify' plugins => 'beautify' !NB 'cardify' without trailers, only cards beauty
         'https://idavlaff.github.io/lampa/plugins/beautify.js',
 // Add new category "4K releases"
         'https://idavlaff.github.io/lampa/plugins/new4k.js'
