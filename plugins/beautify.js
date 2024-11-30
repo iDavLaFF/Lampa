318af45e0e8abfbf59dfe443df32a29549023ff2
iDavLaFF <idavlaff@ya.ru>
2024-11-30 15:13:53 +0300

    Update beautify.js

--- plugins/beautify.js
+++ plugins/beautify.js
@@ -302,9 +302,8 @@
       Lampa.InteractionMain = function (object) {
         var use = new_interface;
         if (!(object.source == 'tmdb' || object.source == 'cub')) use = old_interface;
-        if (window.innerWidth < 767) use = old_interface;
+        if (window.innerWidth < 1080) use = old_interface;
         if (Lampa.Manifest.app_digital < 153) use = old_interface;
-        if (Lampa.Platform.screen('mobile')) use = old_interface;
         return new use(object);
       };
 
