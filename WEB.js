77404d4ae604bcce1961f6cf8c92ef5fb0981ac2
iDavLaFF <idavlaff@ya.ru>
2024-12-01 01:36:06 +0300

    Update WEB.js

--- WEB.js
+++ WEB.js
@@ -2,18 +2,21 @@
     'use strict';
 
     Lampa.Utils.putScriptAsync([
+// @CUB thanks for all yout work
         'http://cub.red/plugin/tmdb-proxy',
         'http://cub.red/plugin/tracks',
+// @LME thanks. Public parsers
         'https://lampame.github.io/main/pubtorr/pubtorr.js',
+// @LME thanks. new categories & collection button for 'franchises'
         'https://lampame.github.io/main/nc/nc.js',
+// @LME thanks. torrents to Infuse saver (iOS/macOS). !NB 'Save all' need 'shortcut': https://www.icloud.com/shortcuts/406d7f6bdfcf466da153a38dea6bb663 
         'https://lampame.github.io/main/its/its.js',
- /* Off 'KPrating' ! don't work with 'cardify', need update.
-        'https://nb557.github.io/plugins/rating.js', */
+// @NB557 thanks. 'KPrating.plugin' !NB Need update 'cause not work with 'cardify'
+        'https://nb557.github.io/plugins/rating.js',
         'https://idavlaff.github.io/lampa/plugins/timecode.js',
+// @CUB.RED thanks. Combine 'interface' & 'cardify' plugins => 'beautify' !NB 'cardify' without trailers, only cards beauty.
         'https://idavlaff.github.io/lampa/plugins/beautify.js',
-/* Combine 'interface' & 'cardify' plugins => 'beautify' ! 'cardify' without trailers, only cards beauty. 
-        'https://idavlaff.github.io/lampa/plugins/interface.js',
-        'https://idavlaff.github.io/lampa/plugins/cardify.js', */
+// Add new category "4K releases"
         'https://idavlaff.github.io/lampa/plugins/new4k.js'
     ], function () { });
 
