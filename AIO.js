5217df15e461fd7f62fd7e329873465021eea24f
iDavLaFF <idavlaff@ya.ru>
2024-12-03 02:08:18 +0300

    Update AIO.js

--- AIO.js
+++ AIO.js
@@ -20,7 +20,7 @@
 // Add new category "4K releases"
         'https://idavlaff.github.io/lampa/plugins/new4k.js',
 // @BWA thanks. Add 'Online.plugin'
-        'https://bwa.to/u421ivt'
+        'https://bwa.to/idavlaff'
     ], function () { });
 
 })();
