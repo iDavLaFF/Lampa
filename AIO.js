8cc95f6678e0111054e3dbb27009502b7ebff631
iDavLaFF <idavlaff@ya.ru>
2024-12-04 03:51:16 +0300

    Update AIO.js

--- AIO.js
+++ AIO.js
@@ -49,13 +49,4 @@
         location.reload()
     }
 
-    Lampa.Storage.listener.follow('change', function (event) {
-        if (event.name == 'activity' && Lampa.Activity.active().component === 'bookmarks') {
-            setTimeout(function(){
-                Lampa.Controller.move('down');
-                Lampa.Controller.move('up');
-            },50)
-        }
-    });
-
 })();
