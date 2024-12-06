ef3d7338ec7dd3b36a268ffa64d9b67cedaabfb6
iDavLaFF <idavlaff@ya.ru>
2024-12-07 00:56:25 +0300

    Update book.js

--- plugins/book.js
+++ plugins/book.js
@@ -1,29 +1,3 @@
- (function () {
-    'use strict';
-
- function bookmarks_folder() {       
-		
-		Lampa.Storage.listener.follow('change', function(event) {
-            if (event.name == 'activity') {
-                if (Lampa.Activity.active().component === 'bookmarks') {
-                    $('.bookmarks-folder__inner').parent().remove();
-                }
-            }
-        });
-		
-}
-
-if (window.appready) bookmarks_folder();
-       else {
-           Lampa.Listener.follow('app', function(e) {
-               if (e.type == 'ready') {
-                  bookmarks_folder();
-               }
-            });
-        }
-
-})();
-
 (function () {
     'use strict';
     Lampa.Platform.tv();
